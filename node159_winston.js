70ed6b60701ea98b73f6629c90e18b3ac9329dfb
vientokk <vientokk@gmail.com>
2023-04-12 01:09:02 +0900

    winston 수정했음

--- node159_winston.js
+++ node159_winston.js
@@ -1,50 +1,137 @@
-const winston = require('winston');
-const winstonDaily = require('winston-daily-rotate-file');
-const moment = require('moment');
+// const winston = require('winston');
+// const winstonDaily = require('winston-daily-rotate-file');  //로그파일 일자별로 생성
+// const appRoot = require('app-root-path'); //app root 경로 
+// const moment = require('process');
 
-function tsFormat() {
-    return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ');
-}
+// const logDir = `${appRoot}/logs`;   //logs 디렉토리 하위에 로그파일 저장
 
-const logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: tsFormat,
-            colorize: true,
-            showlevel: true,
-            level: 'debug',
-        }),
-        new (winstonDaily)({
+// const { combine, timestamp, label, printf } = winston.format;
 
-            level: 'info',
-            filename: 'Log/logs',
-            timestamp: tsFormat,
-            datePattern: '_yyyy-MM-dd.log',
-            showlevel: true,
-            maxsize: 1000000,
-            maxFiles: 5,
-        }),
+// const logFormat = printf(({ level, message, label, timestamp }) => {
+//     return `${timestamp} [${label}] ${level}: ${message}`;  //로그 포맷 
+// });
 
-    ],
-    exceptionHandlers: [
-        new (winstonDaily)({
-            level: 'info',
-            filename: 'Log/exception',
-            timestamp: tsFormat,
-            datePattern: '_yyyy-MM-dd.log',
-            showlevel: true,
-            maxsize: 1000000,
-            maxFiles: 5,
+// const logger = winston.createLogger({
+//     format: combine(
+//         label({
+//             babel: 'System Name'
+//         }),
+//         timestamp({
+//             format: 'YYYY-MM-DD HH:mm:ss'
+//         }),
+//         logFormat
+//     ),
+//     transports: [
+//         new winstonDaily({  //info 레벨 로그 저장할 파일
+//             level: 'info',
+//             datePattern: 'YYYY-MM-DD',
+//             dirname: logDir,
+//             filename: `%DATE%.log`,
+//             maxFiles: 30,    //최근 30일 파일만 저장
+//             zippedArchive: true
+//         }),
+//         new winstonDaily({
+//             level: 'error',
+//             datePattern: 'YYYY-MM-DD',
+//             dirname: logDir,
+//             filename: `%DATE%.log`,
+//             maxFiles: 30,    //최근 30일 파일만 저장
+//             zippedArchive: true
+//         })
+//     ],
+//     exceptionHandlers: [ //uncaughtException 발생시
+//         new winstonDaily({
+//             level: 'error',
+//             datePattern: 'YYYY-MM-DD',
+//             dirname: logDir,
+//             filename: `%DATE%.exception.log`,
+//             maxFiles: 30,    //최근 30일 파일만 저장
+//             zippedArchive: true
+//         })
+//     ]
+// })
 
-        }),
-        new (winston.transports.Console)({
-            timestamp: tsFormat,
-            colorize: true,
-            showlevel: true,
-            level: 'debug',
-        }),
-    ],
+// //운영환경이 아닌경우 콘솔에도 로그 출력
+// if (process.env.NODE_ENV !== 'production') {
+//     logger.add(new winston.transports.Console({
+//         format: winston.format.combine(
+//             winston.format.colorize(),
+//             winston.format.simple()
+//         )
+//     }));
+// }
+
+// module.exports = logger;
+
+
+
+
+
+
+
+
+
+const winston = require('winston'); // winston 모듈
+const winstonDaily = require('winston-daily-rotate-file'); // 로그파일을 일자별로 생성 
+const appRoot = require('app-root-path'); // app root 경로를 가져오는 lib
+const process = require('process');
+
+const logDir = `${appRoot}/logs`; // logs 디렉토리 하위에 로그 파일 저장
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const logFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`; // 로그 출력 포맷 정의
 });
 
-logger.info('인포 로깅');
-logger.error('에러 로깅');
+const logger = winston.createLogger({
+    format: combine(
+        label({
+            lable: 'System Name'
+        }),
+        timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        logFormat
+    ),
+    transports: [
+        new winstonDaily({  // info 레벨 로그를 저장할 파일 설정
+            level: 'info',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: `%DATE%.log`,
+            maxFiles: 30, // 최근 30일치 로그 파일만 저장
+            zippedArchiv: true
+        }),
+        new winstonDaily({
+            level: 'error',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: `%DATE%.error.log`,
+            maxFiles: 30, // 최근 30일치 로그 파일만 저장
+            zippedArchiv: true
+        })
+    ],
+    exceptionHandlers: [  // uncaughtException 발생시
+        new winstonDaily({
+            level: 'error',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: `%DATE%.exception.log`,
+            maxFiles: 30, // 최근 30일치 로그 파일만 저장
+            zippedArchiv: true
+        })
+    ]
+})
+
+// 운영 환경이 아닌 경우 콘솔로도 로그 출력
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize(), // 색상 넣어서 출력
+            winston.format.simple() // 간단한 포맷으로 출력
+        )
+    }));
+}
+
+module.exports = logger;
